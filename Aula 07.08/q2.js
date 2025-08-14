c07a95b80c4bdba79ec3f60878a6675fc4aac1db
𝑭𝒍𝒂𝒖𝒔𝒊𝒐𝑵𝒖𝒏𝒆𝒔 <flausio.nunes@estudante.iftm.edu.br>
2025-08-14 14:19:53 -0300

    Add files via upload

---
+++ Aula 07.08/q2.js
@@ -0,0 +1,14 @@
+//N - n1, n2
+//S - soma
+//b - subtração
+//M - multiplicação
+
+let n1 = prompt("Informe seu numero 1: ");
+let n2 = prompt("Informe seu numero 2: ");
+
+n1 = Number (n1);
+n2 = Number (n2);
+
+console.log("A soma desses numeros são: " + (n1+n2));
+console.log("A subtração desses numeros são: " + (n1-n2));
+console.log("A multiplicação desses numeros são: " + (n1*n2));
